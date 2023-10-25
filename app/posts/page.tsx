bc9583c64d4d8fe50001644ef3b0d76f5e41b0e0
00andobe <00andobe@skola.boras.se>
2023-10-25 03:39:02 +0200

    fixed api route to fetch all posts

---
+++ app/posts/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Post } from "@/interfaces";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import "../../styles/posts.css";
+
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const result = await fetch("/api/posts");
+      const postsFromApi = await result.json();
+      setPosts(postsFromApi);
+    };
+    getPosts();
+  }, []);
+
+  return (
+    <div className="post-list">
+      {posts.map((post: Post) => (
+        <Link key={post.id} href={`/posts/${post.id}`}>
+          <div className="post">
+            <h2 className="post-title">{post.title + " by " + post.author}</h2>
+            <div className="post-content" dangerouslySetInnerHTML={{ __html: post.content }} />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
